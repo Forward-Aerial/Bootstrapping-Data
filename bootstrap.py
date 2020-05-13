f05c92cdcd7c0852245a3e21a9c8cf44c207d2ef
Gabriel Britain <quontas@gmail.com>
2020-05-13 13:19:32 -0500

    Functional bootstrapping.

--- bootstrap.py
+++ bootstrap.py
@@ -1,15 +1,61 @@
 import argparse
+import datetime
 import glob
+import json
+import logging
 import os
+import shutil
+from typing import Dict, List, Tuple
 
 import torch
 import torchvision
-from PIL import Image
-from torchvision import transforms
 from matplotlib import pyplot as plt
 from matplotlib import patches
+from PIL import Image
+from torchvision import transforms
 
-LOOKUP = {2: "Captain Falcon", 6: "Fox"}
+
+def construct_annotation(
+    image_id: int,
+    annotation_id: int,
+    box: Tuple[float, float, float, float],
+    label: int,
+    categories: List[Dict],
+) -> Dict:
+    min_x, min_y, max_x, max_y = box
+    width = max_x - min_x
+    height = max_y - min_y
+    points = [
+        min_x,  # Top Left
+        min_y,
+        min_x + width,  # Top Right
+        min_y,
+        min_x + width,  # Bottom Right
+        min_y + height,
+        min_x,
+        min_y + height,  # Bottom Left
+    ]
+
+    return {
+        "iscrowd": 0,
+        "image_id": image_id,
+        "bbox": [min_x, min_y, width, height],
+        "category_id": label,
+        "segmentation": [points],
+        "id": annotation_id,
+        "area": width * height,
+    }
+
+
+def load_coco_data(annotation_path: str):
+    with open(annotation_path, "r") as json_file:
+        coco_data = json.load(json_file)
+    licenses = coco_data["licenses"]
+    images = coco_data["images"]
+    info = coco_data["info"]
+    categories = coco_data["categories"]
+    annotations = coco_data["annotations"]
+    return licenses, images, info, categories, annotations
 
 
 def display_image(img: Image, prediction):
@@ -28,33 +74,111 @@ def display_image(img: Image, prediction):
             (min_x, min_y), width, height, linewidth=1, edgecolor="r", facecolor="none",
         )
         ax.add_patch(rect)
-        ax.text(min_x, min_y, LOOKUP[label.item()], c="r")
+        ax.text(min_x, min_y, "Captain Falcon" if label.item() == 2 else "Fox", c="r")
     fig.show()
     plt.waitforbuttonpress()
     plt.close(fig)
 
 
-def bootstrap(image_root: str, model_file: str):
-    image_files = glob.iglob(os.path.join(image_root, "*"))
-    model: torchvision.models.detection.FasterRCNN = torch.load("detector.pt")
+def bootstrap(
+    existing_image_dir: str,
+    existing_annotation_path: str,
+    new_images_dir: str,
+    model_path: str,
+):
+    licenses, images, info, categories, annotations = load_coco_data(
+        existing_annotation_path
+    )
+
+    new_image_paths = glob.iglob(os.path.join(new_images_dir, "*"))
+    model: torchvision.models.detection.FasterRCNN = torch.load(model_path)
 
     to_tensor = transforms.ToTensor()
-    for image_file in image_files:
-        img = Image.open(image_file)
+
+    max_image_id = max(images, key=lambda x: x["id"])["id"]
+
+    max_annotation_id = max(annotations, key=lambda x: x["id"])["id"]
+    annotation_id = max_annotation_id + 1
+    for i, image_path in enumerate(new_image_paths):
+        image_id = max_image_id + i
+        logging.info(f"Processing {image_path}")
+
+        # Copy it to the existing images directory using the ID as a filename
+        new_image_location = os.path.join(existing_image_dir, f"{image_id}.jpg")
+        logging.debug(f"Copying from {image_path} to {new_image_location}")
+        shutil.copyfile(image_path, new_image_location)
+
+        # Update the variable so the rest proceeds as expected.
+        image_path = new_image_location
+
+        # Add the image to the list of images
+        img: Image = Image.open(image_path)
+        width, height = img.size
+        images.append(
+            {
+                "date_captured": 0,
+                "flickr_url": 0,
+                "height": height,
+                "width": width,
+                "id": image_id,
+                "license": 0,
+                "file_name": f"{image_id}.jpg",
+            }
+        )
+        img = Image.open(image_path)
         img_tensor = to_tensor(img).to("cuda")
         prediction = model([img_tensor])
-        display_image(img, prediction)
+        # display_image(img, prediction)
+        boxes = prediction[0]["boxes"]
+        labels = prediction[0]["labels"]
+        scores = prediction[0]["scores"]
+        for box, label, score in zip(boxes, labels, scores):
+            if score.item() <= 0.7:
+                continue
+            annotation = construct_annotation(
+                image_id, annotation_id, box.tolist(), label.item(), categories,
+            )
+            annotations.append(annotation)
+            annotation_id += 1
+
+    with open(existing_annotation_path, "w") as annotation_file:
+        json.dump(
+            {
+                "licenses": licenses,
+                "images": images,
+                "annotations": annotations,
+                "info": info,
+                "categories": categories,
+            },
+            annotation_file,
+        )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "image_root",
+        "existing_images",
         type=str,
-        help="The directory that contains all of the image to generate annotations for.",
+        help="Path to the directory containing existing COCO images",
     )
     parser.add_argument(
-        "model_file", type=str, help="Path to the PyTorch FasterRCNN model file."
+        "existing_annotations",
+        type=str,
+        help="Path to the file containing existing COCO annotations",
+    )
+    parser.add_argument(
+        "new_images",
+        type=str,
+        help="Path to the directory containing new images to label",
+    )
+    parser.add_argument(
+        "model_file", type=str, help="Path to a trained PyTorch FasterRCNN model file."
     )
     args = parser.parse_args()
-    bootstrap(args.image_root, args.model_file)
+    logging.basicConfig(level=logging.DEBUG)
+    bootstrap(
+        args.existing_images,
+        args.existing_annotations,
+        args.new_images,
+        args.model_file,
+    )
